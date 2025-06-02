a8b81b8a69feed489f6475920ee0383a1d727c31
Alelanderos <patoybob13@gmail.com>
2025-06-02 14:24:55 -0600

    virutal lab

--- src/App.tsx
+++ src/App.tsx
@@ -23,6 +23,7 @@ const App = () => (
           <Route path="/" element={<AppLayout><Dashboard /></AppLayout>} />
           <Route path="/dashboard" element={<AppLayout><Dashboard /></AppLayout>} />
           <Route path="/simulations" element={<AppLayout><Simulations /></AppLayout>} />
+          <Route path="/virtual lab" element={<AppLayout><div className="p-6">Virutal lab Page (Coming Soon)</div></AppLayout>} />
           <Route path="/products" element={<AppLayout><Products /></AppLayout>} />
           <Route path="/products/:id" element={<AppLayout><ProductView /></AppLayout>} />
           <Route path="/ingredients" element={<AppLayout><div className="p-6">Ingredients Page (Coming Soon)</div></AppLayout>} />
