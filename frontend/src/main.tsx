5c7ca4a65b789b103870da2d4928142907dbda31
Alelanderos <patoybob13@gmail.com>
2025-06-03 17:09:04 -0600

    front

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,10 +5,7 @@ import {BrowserRouter as Router} from 'react-router-dom'
 
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-    <Router>
-        <React.StrictMode>
             <App />
-        </React.StrictMode>
-    </Router>
+
 );
 
