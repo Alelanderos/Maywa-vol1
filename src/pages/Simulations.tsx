cdc5f9e9eb8486bdfe7b28e35ef366d3802934ad
Alelanderos <patoybob13@gmail.com>
2025-06-03 23:50:54 -0600

    z

--- src/pages/Simulations.tsx
+++ src/pages/Simulations.tsx
@@ -90,7 +90,6 @@ const Simulations = () => {
         />
       </div>
       
-      <div className="grid grid-cols-1"><ChartAreaInteractive /></div>
 
       <div className="mt-8 grid grid-cols-1 lg:grid-cols-3 gap-4">
         <div className="lg:col-span-2">
