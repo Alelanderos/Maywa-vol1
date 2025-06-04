af6ae2967640830944d751ef184e1f7059be1a99
Alelanderos <patoybob13@gmail.com>
2025-06-03 19:20:32 -0600

    front

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,6 @@
-import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import {BrowserRouter as Router} from 'react-router-dom'
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-    <Router>
-        <React.StrictMode>
-         <App />
-        </React.StrictMode>,
-    </Router>
-    );
+createRoot(document.getElementById("root")!).render( <App />);
  
