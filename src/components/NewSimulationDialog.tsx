49bb598669d6e9a044a906f1f0c1ae89343b892e
Alelanderos <patoybob13@gmail.com>
2025-06-04 19:17:44 -0600

    prubeas de backend

--- src/components/NewSimulationDialog.tsx
+++ src/components/NewSimulationDialog.tsx
@@ -101,149 +101,33 @@ const onSubmit = async (data: SimulationData) => {
           <div className="space-y-4">
             <Form {...form}>
               <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
-                <FormField
-                  control={form.control}
-                  name="name"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Simulation Name</FormLabel>
-                      <FormControl>
-                        <Input placeholder="Enter simulation name" {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                
-                <FormField
-                  control={form.control}
-                  name="biomasa"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Biomasa Inicial </FormLabel>
-                      <FormControl>
-                        <Input 
-                          type="number" 
-                          step="1.0"
-                          placeholder="0.0" 
-                          {...field}
-                          onChange={(e) => field.onChange(Number(e.target.value))}
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-
-                <FormField
-                  control={form.control}
-                  name="sustrato"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Sustrato Inicial </FormLabel>
-                      <FormControl>
-                        <Input 
-                          type="number" 
-                          step="1.0"
-                          placeholder="0.0" 
-                          {...field}
-                          onChange={(e) => field.onChange(Number(e.target.value))}
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-
-                <FormField
-                  control={form.control}
-                  name="nitrogeno"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Nitrogeno Inicial </FormLabel>
-                      <FormControl>
-                        <Input 
-                          type="number" 
-                          step="1.0"
-                          placeholder="0.0" 
-                          {...field}
-                          onChange={(e) => field.onChange(Number(e.target.value))}
-                        />      
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-
-                <FormField
-                  control={form.control}
-                  name="temperature"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Temperatura (°C)</FormLabel>
-                      <FormControl>
-                        <Input 
-                          type="number" 
-                          placeholder="25" 
-                          {...field}
-                          onChange={(e) => field.onChange(Number(e.target.value))}
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                
-
-                <FormField
-                  control={form.control}
-                  name="pH"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>pH </FormLabel>
-                      <FormControl>
-                        <Input 
-                          type="number" 
-                          placeholder="7" 
-                          {...field}
-                          onChange={(e) => field.onChange(Number(e.target.value))}
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                
-                <FormField
-                  control={form.control}
-                  name="time"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Duracion (horas)</FormLabel>
-                      <FormControl>
-                        <Input 
-                          type="number" 
-                          placeholder="24" 
-                          {...field}
-                          onChange={(e) => field.onChange(Number(e.target.value))}
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
+                {/* Form Fields... */}
+                {['name', 'biomasa', 'sustrato', 'nitrogeno', 'temperature', 'pH', 'time'].map((field) => (
+                  <FormField
+                     key={field}
+                     control={form.control}
+                     name={field as keyof SimulationData}
+                     render={({ field: innerField }) => (
+                      <FormItem>
+                        <FormLabel>{field.charAt(0).toUpperCase() + field.slice(1)}</FormLabel>
+                        <FormControl>
+                          <Input type={typeof form.getValues()[field] === 'number' ? 'number' : 'text'}
+                                 {...innerField}
+                                 onChange={(e) => innerField.onChange(
+                                   typeof form.getValues()[field] === 'number'
+                                     ? Number(e.target.value)
+                                     : e.target.value
+                                 )} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                ))}
                 
                 <div className="flex gap-2 pt-4">
-                  <Button type="submit" className="flex-1">
-                    Start Simulation
-                  </Button>
-                  <Button 
-                    type="button" 
-                    variant="outline" 
-                    onClick={() => setOpen(false)}
-                  >
-                    Cancel
-                  </Button>
+                  <Button type="submit" className="flex-1">Start Simulation</Button>
+                  <Button type="button" variant="outline" onClick={() => setOpen(false)}>Cancel</Button>
                 </div>
               </form>
             </Form>
